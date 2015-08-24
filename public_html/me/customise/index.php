684307bd3e3d7fb8d39994114a3f80ac57086146
Damon <dittopower@gmail.com>
2015-08-24 10:28:02 +1000

    customiser v1.1
    i made this awhile ago.
    just updated to enforce.

---
+++ public_html/me/customise/index.php
@@ -0,0 +1,13 @@
+<?php //Load Template
+	$home = $_SERVER['DOCUMENT_ROOT']."/";
+	require_once $home."page.php";
+	require_once $home."../core/media.php";
+
+	enforce_user();
+	echo "<h1>Custom CSS</h1>";
+	echo "<p>Wanna customise your experience? Then upload your own css.</p>";
+	media_form("", "custom.css");
+	if(file_exists($home.$custom['custom.css'])){
+		echo "<p>Edit your CSS: <a href='//deamon.info/edit?url=".$custom['custom.css']."'>custom.css</a> </p>";
+	}
+?>
--- public_html/me/index.php
+++ public_html/me/index.php
@@ -5,5 +5,7 @@
 <!-- START content -->
 
 user info page coming sometime
+<br>
+Dont like my colour scheme <a href='./customise'>customise</a> it.
 
 <!-- END content -->
