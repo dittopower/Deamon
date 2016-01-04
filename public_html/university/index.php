5c399e19e3d6057b839e7e4397dbc0029dc95655
Deamon <dittopower@gmail.com>
2016-01-04 20:57:19 +1000

    uni info

--- public_html/university/index.php
+++ public_html/university/index.php
@@ -10,19 +10,23 @@
 	<hr>
 	<?php
 		$projects = scandir('./');
-		$fileex = "(.php)|(.htm.*)|(.jpg)|(.png)|(.gif)";
+		$fileex = "(.php)|(.htm.*)";
 		$folder = "(^[^.]+$)";
 		foreach ($projects as $project){
-			if (preg_match("/$fileex|$folder/",$project)&& !(preg_match("/index($fileex)|302MEDIA|cgi-bin/",$project))){
+			if (preg_match("/$fileex|$folder/",$project)&& !(preg_match("/index($fileex)|302MEDIA|cgi-bin|302/",$project))){
 				$p = preg_replace("/$fileex/","",$project);
+				echo "<span class='obj'  style='width:96%;text-align:center;'><a href='$project'><h3>$p</h3></a>";
+				if (file_exists("$p.png")){
+					echo "<img src='./$p.png' class='project_banner' alt='$p Banner' style='width:50%;'>";
+				}
+				echo "<br><p style='text-align:justify;'>";
 				if (file_exists("$p.info")){
 					$text = file_get_contents("$p.info");
+					echo $text;
 				}else{
-					$text = $p;
+					echo "Project .info Not Found.";
 				}
-				$text = htmlescape($text);
-				echo "<a href='$project' class='obj' title='$text'>$p</a>";
+				echo "</p></span>";
 			}
 		}
 	?>
-</div>
